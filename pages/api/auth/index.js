ba71c845ff71b84e6d36372737b4edbf9a1588b3
wahyu <wahyupradit@gmail.com>
2022-03-31 23:30:56 +0700

    create login data to RESTApi

---
+++ pages/api/auth/index.js
@@ -0,0 +1,32 @@
+const baseUrl = process.env.BASE_URL;
+
+export default async function handler(req, res) {
+  const header = req.headers
+  const tokenName = header.origin
+
+  if (`${tokenName}/` == baseUrl && req.method == 'POST') {
+    const fetchData = await fetch("https://dev-457931.okta.com/oauth2/aushd4c95QtFHsfWt4x6/v1/token", {
+      method: req.method,
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        client_id: '0oahdhjkutaGcIK2M4x6',
+        username: req.query.username,
+        password: req.query.password,
+        grant_type: 'password',
+        scope: 'offline_access'
+      })
+    });
+    const response = await fetchData.json();
+    const data = response;
+    res.status(200).json(data);
+  } else {
+    const error = [{
+      error: true,
+      message: 'No Data!'
+    }]
+    res.status(500).json(error);
+  }
+}
--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,30 @@
 import Layout from "../components/layout/layout.component"
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import Link from "next/link";
+import { useState } from "react";
+import { loginAuth } from '../lib/auth/login';
 
 function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChangeLogin = (e) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({
+      ...formData,
+      [name]: value
+    }));
+  };
+
+  const handleSumbitLogin = (e) => {
+    e.preventDefault();
+    loginAuth(formData).then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <Layout title="LOGIN">
       <Container>
@@ -10,15 +32,15 @@ function Login() {
           <Col md={5}>
             <h3 className="mb-2 text-center">Login</h3>
             <hr />
-            <Form>
+            <Form onSubmit={handleSumbitLogin}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Label>User Name</Form.Label>
+                <Form.Control name="username" type="text" placeholder="User Name" onChange={handleChangeLogin} />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control name="password" type="password" placeholder="Password" onChange={handleChangeLogin} />
               </Form.Group>
               <Button variant="success" type="submit">
                 Login
